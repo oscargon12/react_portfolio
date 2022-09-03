97216011c241efb4040dcdaa9cd9c0fdf31b0d7b
oscargon12 <oscargon12@gmail.com>
2022-09-03 00:06:19 -0500

    figma tokens / Style dictionary

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Header } from './components/header/header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Fragment>
+        <Header/>
+      </Fragment>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
---
+++ src/components/header/header.jsx
@@ -0,0 +1,3 @@
+export function Header(){
+    return <h2>Esto es un header</h2>
+}
