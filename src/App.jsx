2c5434b2cb0b1fa2060fbbca815fe9d2217750b4
oscargon12 <oscargon12@gmail.com>
2022-11-08 18:51:28 -0500

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import './components/scss/main.scss'
 import NavBar from './components/layout/navBar/NavBar'
+import { Hero } from './components/HeroSection/Hero'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer'
 
 
@@ -13,6 +14,7 @@ function App() {
     <>
 
       <NavBar link1="My work" link2="About" link3="Tools" link4="Contact" />
+      <Hero/>
       <ItemListContainer greetting="Holaaaa"/>
       
     </>
---
+++ src/components/HeroSection/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <>
+        <section className='align-left primary-font'>
+            <h2>Hello, I', Oscar González</h2>
+            <h3>I'm a frontend developer</h3>
+            <h5>I participate in the creation of projectsthat are efficient and intiuitive for the user. I'm a hamburguer and science fiction lover</h5>
+        </section>
+        <section>
+            
+        </section>
+    </>
+  )
+}
