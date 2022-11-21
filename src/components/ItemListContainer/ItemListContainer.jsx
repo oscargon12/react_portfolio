191405f1c49d52623d20563201b4b00bdb6d8e28
oscargon12 <oscargon12@gmail.com>
2022-11-20 23:53:54 -0500

    itemListContainer

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { getProjects } from '../../helpers/getData'
+//import { getProjects } from '../../helpers/getData'
 import { ItemList } from '../ItemList/ItemList'
 import { Loader } from '../Loader/Loader'
-import { collection, doc, getDocs } from 'firebase/firestore/lite'
+import { collection, getDocs, query, where } from 'firebase/firestore/lite'
 import { db } from '../../firebase/config'
+import { useParams } from 'react-router-dom'
 
 export const ItemListContainer = (props) => {
 
   const [loading, setLoading] = useState(false)
   const [projects, setProjects] = useState([])
+
+  //const {catId} = useParams() //for query filter
 
   console.log(projects);
 
@@ -18,6 +21,7 @@ export const ItemListContainer = (props) => {
     
     //1. armar referencia a la coleccion
     const projectsRef = collection(db, 'projects')
+    //const q = catId ? query(projectsRef, where('category', '==', catId)) : projectsRef //Query filter
 
     //2. GET a esa ref
     getDocs(projectsRef)
